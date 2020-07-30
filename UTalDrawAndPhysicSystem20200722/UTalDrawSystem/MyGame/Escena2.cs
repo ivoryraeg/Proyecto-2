ab7d66de91839c833c58b1494981a801739932a1
ivoryraeg <68978393+ivoryraeg@users.noreply.github.com>
2020-07-29 21:40:17 -0400

    Initial commit

---
+++ UTalDrawAndPhysicSystem20200722/UTalDrawSystem/MyGame/Escena2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTalDrawSystem.SistemaDibujado;
+using UTalDrawSystem.SistemaGameObject;
+
+namespace UTalDrawSystem.MyGame
+{
+    public class Escena2 : Escena
+    {
+        public Escena2()
+        {
+            UTGameObjectsManager.Init();
+            new Gato("Auto", new Vector2(300, 300), 1, UTGameObject.FF_form.Circulo);
+            //new UTGameObject("meow_cookie", new Vector2(700, 500), 1, UTGameObject.FF_form.Circulo);
+            new UTGameObject("Muro", new Vector2(1000, 500), 1, UTGameObject.FF_form.Rectangulo, true);
+            //new UTGameObject("Muro", new Vector2(500, 800), 1, UTGameObject.FF_form.Rectangulo, true);
+            new UTGameObject("Muro", new Vector2(500, 500), 1, UTGameObject.FF_form.Rectangulo, true);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.D1))
+            {
+                new EscenaInicial();
+            }
+        }
+    }
+}
---
+++ UTalDrawAndPhysicSystem20200722/UTalDrawSystem/SistemaDibujado/Escena.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTalDrawSystem.SistemaDibujado
+{
+    public class Escena
+    {
+        public List<Dibujable> dibujables { get; private set; } = new List<Dibujable>();
+        public static Escena INSTANCIA;
+
+        public Escena()
+        {
+            INSTANCIA = this;
+        }
+        public void agregarDib(Dibujable dib)
+        {
+            dibujables.Add(dib);
+        }
+        public void removerDib(Dibujable dib)
+        {
+            dibujables.Remove(dib);
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
